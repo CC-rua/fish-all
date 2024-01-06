a6baaf1e6d340d20287dc20b997e61cdd7c18316
zhujiaqi <zhujiaqi@qq.com>
2024-01-06 14:58:35 +0800

    Initial commit

---
+++ fish-base/src/main/java/com/fish/Main.java
@@ -0,0 +1,11 @@
+package com.fish;
+
+/**
+ * @author: cc
+ * @date: 2024/1/6
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
